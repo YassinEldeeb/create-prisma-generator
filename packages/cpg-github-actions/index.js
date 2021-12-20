db3282c076911dcaa70f5004c2a129d23e485df9
YassinEldeeb <yassineldeeb94@gmail.com>
2021-12-20 21:45:03 +0200

    some cleanup

--- packages/cpg-github-actions/index.js
+++ packages/cpg-github-actions/index.js
@@ -5,7 +5,4 @@ import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
-copyTemplate(
-  path.join(__dirname, `./template/.github`),
-  `${process.argv[2]}/.github`,
-)
+copyTemplate(path.join(__dirname, `./template`), `${process.argv[2]}`)
