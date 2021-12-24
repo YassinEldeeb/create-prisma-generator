5d55ed7fcee92ba80c5512167c3cd211e5f8c111
YassinEldeeb <yassineldeeb94@gmail.com>
2021-12-24 21:45:44 +0200

    fix(cpg-template): testing workflow

--- packages/cpg-template/index.js
+++ packages/cpg-template/index.js
@@ -3,6 +3,7 @@ import path, { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import fse from 'fs-extra'
 
+console.log('a change to deploy')
 const __dirname = dirname(fileURLToPath(import.meta.url))
 fse.copySync(
   path.join(path.join(__dirname, `./template`)),
