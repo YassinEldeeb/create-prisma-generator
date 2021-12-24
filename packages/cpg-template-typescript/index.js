1d68d83837fd5d6b68882e525892e44b54390226
YassinEldeeb <yassineldeeb94@gmail.com>
2021-12-24 20:43:34 +0200

    fix: remove debug line

--- packages/cpg-template-typescript/index.js
+++ packages/cpg-template-typescript/index.js
@@ -3,8 +3,6 @@ import path, { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import fse from 'fs-extra'
 
-console.log('Make change to package root to debug CI')
-
 const __dirname = dirname(fileURLToPath(import.meta.url))
 fse.copySync(
   path.join(path.join(__dirname, `./template`)),
