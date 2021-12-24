cb3ce7b096b5be6baefa0731c9df263159c99d4e
YassinEldeeb <yassineldeeb94@gmail.com>
2021-12-24 06:10:06 +0200

    feat: guide dependabot to templates programmatically

---
+++ scripts/guideDependabot.ts
@@ -0,0 +1,36 @@
+import fs from 'fs'
+import path from 'path'
+
+const dependabotTemplate = (path: string) => `  - package-ecosystem: 'npm'
+    directory: '${path}'
+    schedule:
+      interval: 'weekly'
+`
+const packagesPath = path.join(process.cwd(), 'packages')
+const packages = fs
+  .readdirSync(packagesPath, { withFileTypes: true })
+  .filter((dirent) => dirent.isDirectory())
+
+const dependabotConfig: string[] = []
+const dependabotYMLPath = path.join(process.cwd(), '.github/dependabot.yml')
+packages.forEach((pkg) => {
+  const folders = fs
+    .readdirSync(path.join(packagesPath, pkg.name), { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+
+  if (folders.find((e) => e.name === 'template')) {
+    dependabotConfig.push(
+      dependabotTemplate('/packages/' + pkg.name + '/template'),
+    )
+  }
+})
+
+const placeholder = '# ADDED PROGRAMATICALLY'
+const dependabotFile = fs.readFileSync(dependabotYMLPath, 'utf-8')
+fs.writeFileSync(
+  dependabotYMLPath,
+  (dependabotFile.split(placeholder)[0] + placeholder).replace(
+    placeholder,
+    `${placeholder}\n` + dependabotConfig.join('\n'),
+  ),
+)
