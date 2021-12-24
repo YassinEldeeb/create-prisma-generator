d11447e16b7cadebccb354cce195ff446cda58db
YassinEldeeb <yassineldeeb94@gmail.com>
2021-12-24 04:59:41 +0200

    avoiding the haslle by converting the script to JS

--- scripts/simulateDist.js
+++ scripts/simulateDist.js
@@ -1,5 +1,5 @@
-import fs from 'fs'
-import path from 'path'
+const fs = require('fs')
+const path = require('path')
 
 const cliPath = path.join(__dirname, '../packages/create-prisma-generator')
 fs.mkdirSync(path.join(cliPath, 'dist'))
