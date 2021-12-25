d8d6c402d984aa479b3f49884f37204b71538e40
YassinEldeeb <yassineldeeb94@gmail.com>
2021-12-25 12:56:50 +0200

    fix: publish cpg-template-typescript

--- packages/cpg-template-typescript/index.js
+++ packages/cpg-template-typescript/index.js
@@ -3,6 +3,8 @@ import path, { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import fse from 'fs-extra'
 
+console.log('change to publish')
+
 const __dirname = dirname(fileURLToPath(import.meta.url))
 fse.copySync(
   path.join(path.join(__dirname, `./template`)),
