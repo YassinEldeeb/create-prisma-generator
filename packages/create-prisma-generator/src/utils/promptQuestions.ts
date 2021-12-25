53657994c2177c38911a19c89276c2a0226c1946
YassinEldeeb <yassineldeeb94@gmail.com>
2021-12-25 16:15:25 +0200

    fix(create-prisma-generator): lowercase prompt questions

--- packages/create-prisma-generator/src/utils/promptQuestions.ts
+++ packages/create-prisma-generator/src/utils/promptQuestions.ts
@@ -8,7 +8,7 @@ export const promptQuestions = async (): Promise<Answers> => {
     {
       type: 'input',
       name: 'generatorName',
-      message: "What's your generator name",
+      message: "what's your generator name",
       // Validate Package Name
       validate(pkgName) {
         const validPkgName = validatePkgName(pkgName)
@@ -25,24 +25,24 @@ export const promptQuestions = async (): Promise<Answers> => {
     {
       type: 'confirm',
       name: 'typescript',
-      message: 'Do you want to use Typescript',
+      message: 'do you want to use Typescript',
     },
     {
       type: 'list',
       name: 'packageManager',
-      message: 'Which package manager do you want to use',
+      message: 'which package manager do you want to use',
       choices: ['yarn', 'pnpm', 'npm'],
       default: 'pnpm',
     },
     {
       type: 'confirm',
       name: 'githubActions',
-      message: 'Automate publishing the generator with Github Actions',
+      message: 'automate publishing the generator with Github Actions',
     },
     {
       type: 'confirm',
       name: 'semanticRelease',
-      message: '(Github Actions) Setup automatic semantic release',
+      message: '(Github Actions) setup automatic semantic release',
       when: (answers: Answers) => {
         return answers.githubActions
       },
@@ -50,7 +50,7 @@ export const promptQuestions = async (): Promise<Answers> => {
     {
       type: 'confirm',
       name: 'usageTemplate',
-      message: 'Setup workspace for testing the generator',
+      message: 'setup workspace for testing the generator',
     },
   ]
   const answers = (await inquirer.prompt(questions)) as Answers
