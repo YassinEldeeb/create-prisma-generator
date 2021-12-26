0ff443929f6285030173ef160fa53ce800108957
YassinEldeeb <yassineldeeb94@gmail.com>
2021-12-26 03:54:39 +0200

    chore: add tags

---
+++ scripts/generateTags.ts
@@ -0,0 +1,25 @@
+import fs from 'fs'
+import path from 'path'
+import { exec, execSync } from 'child_process'
+
+const packagesPath = path.join(process.cwd(), 'packages')
+
+const packages = fs
+  .readdirSync(packagesPath, { withFileTypes: true })
+  .filter((dirent) => dirent.isDirectory())
+
+packages.forEach((pkg) => {
+  const packageJSON = JSON.parse(
+    fs.readFileSync(path.join(packagesPath, pkg.name, 'package.json'), 'utf-8'),
+  )
+
+  if (!packageJSON.private) {
+    exec(
+      `npm view ${packageJSON.name} version`,
+      function (error, stdout, stderr) {
+        const pkgVersion = stdout.trim()
+        execSync(`git tag ${packageJSON.name}-v${pkgVersion}`)
+      },
+    )
+  }
+})
