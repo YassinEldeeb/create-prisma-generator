3ab271141507ca47065da97d60ab3c705f66aace
YassinEldeeb <yassineldeeb94@gmail.com>
2021-12-29 16:01:58 +0200

    fix: debug CI

--- scripts/ci/utils/updatePackageVersion.ts
+++ scripts/ci/utils/updatePackageVersion.ts
@@ -1,6 +1,7 @@
 import { execSync } from 'child_process'
 
 export const updatePackageVersion = (cwd: string, nextVersion: string) => {
+  console.log(cwd)
   execSync(`npm --no-git-tag-version version ${nextVersion}`, {
     cwd,
   })
