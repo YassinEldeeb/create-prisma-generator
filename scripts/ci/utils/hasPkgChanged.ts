b90126446febeffafebc5da0fa433ba36b88ac72
YassinEldeeb <yassineldeeb94@gmail.com>
2021-12-30 06:36:10 +0200

    fix(CI): debug hasPkgChanged

--- scripts/ci/utils/hasPkgChanged.ts
+++ scripts/ci/utils/hasPkgChanged.ts
@@ -1,6 +1,14 @@
 import { execSync } from 'child_process'
 
 export const hasPkgChanged = (folder: string) => {
+  console.log(
+    execSync(`git diff HEAD^ HEAD --name-only`)
+      .toString()
+      .trim()
+      .split('\n')
+      .find((e) => e.includes(folder)),
+  )
+  console.log(folder)
   return !!execSync(`git diff HEAD^ HEAD --name-only`)
     .toString()
     .trim()
