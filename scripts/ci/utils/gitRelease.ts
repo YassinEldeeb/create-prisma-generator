53d6b76d37fb84abb37f57ceb9e150df45216955
YassinEldeeb <yassineldeeb94@gmail.com>
2021-12-30 03:07:00 +0200

    fix(Publsh CI): skip husky

--- scripts/ci/utils/gitRelease.ts
+++ scripts/ci/utils/gitRelease.ts
@@ -3,7 +3,7 @@ import { execSync } from 'child_process'
 export const gitRelease = (nextTag: string) => {
   const releaseMessage = `chore(release): ${nextTag} [skip ci]`
   execSync(`git add -A .`)
-  execSync(`git commit -m"${releaseMessage}"`)
+  execSync(`git commit --no-verify -m"${releaseMessage}"`)
   execSync(`git tag -a ${nextTag} HEAD -m"${releaseMessage}"`)
-  execSync(`git push --follow-tags origin main`)
+  execSync(`git push --no-verify --follow-tags origin main`)
 }
