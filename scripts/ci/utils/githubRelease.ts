1421b586fc9a7ee3576d0f514897655421db2fcb
YassinEldeeb <yassineldeeb94@gmail.com>
2021-12-30 18:11:49 +0200

    fix(publish-ci): curl post request

--- scripts/ci/utils/githubRelease.ts
+++ scripts/ci/utils/githubRelease.ts
@@ -15,8 +15,9 @@ export const githubRelease = (
     owner: GIT_COMMITTER_NAME,
   })
 
+  // Source: https://docs.github.com/en/rest/reference/releases#create-a-release
   execSync(
-    `curl -u ${GIT_COMMITTER_NAME}:${GITHUB_TOKEN} -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${repoName}/releases -d ${releaseData}`,
+    `curl -X POST -u ${GIT_COMMITTER_NAME}:${GITHUB_TOKEN} -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${repoName}/releases -d '${releaseData}'`,
   )
 
   logger.success(`Published a new release with tag ${tag}!`)
