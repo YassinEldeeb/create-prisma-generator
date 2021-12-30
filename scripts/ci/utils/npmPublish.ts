c8fc319f03e7edb3e6482dafaed8d6731ebe90ac
YassinEldeeb <yassineldeeb94@gmail.com>
2021-12-30 04:45:33 +0200

    fix(CI): auth npm properly

--- scripts/ci/utils/npmPublish.ts
+++ scripts/ci/utils/npmPublish.ts
@@ -1,9 +1,6 @@
 import { execSync } from 'child_process'
 
 export const npmPublish = (cwd: string) => {
-  execSync(
-    `NPM_TOKEN=${process.env.NPM_TOKEN} npm publish --no-git-tag-version`,
-    { cwd },
-  )
+  execSync(`npm publish --no-git-tag-version`, { cwd })
   console.log('Published the package successfully!')
 }
