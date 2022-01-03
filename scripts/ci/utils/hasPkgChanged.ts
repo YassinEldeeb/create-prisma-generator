53d8ab1ee76676aeb83004538df947ce327238fd
YassinEldeeb <yassineldeeb94@gmail.com>
2022-01-03 14:42:16 +0200

    fix: hasPkgChanged logic

--- scripts/ci/utils/hasPkgChanged.ts
+++ scripts/ci/utils/hasPkgChanged.ts
@@ -1,10 +1,14 @@
 import { execSync } from 'child_process'
+import path from 'path'
 
 // TODO: Must ignore files in __tests__ when deciding
 export const hasPkgChanged = (folder: string) => {
+  console.log(execSync(`git diff HEAD^ HEAD --name-only`).toString())
   return !!execSync(`git diff HEAD^ HEAD --name-only`)
     .toString()
     .trim()
     .split('\n')
     .find((e) => e.includes(folder))
 }
+
+console.log(hasPkgChanged('./packages/cpg-github-actions'))
