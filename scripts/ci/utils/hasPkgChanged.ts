ee2c9119a9b1d40db660afb9bf166e3b57a49048
YassinEldeeb <yassineldeeb94@gmail.com>
2022-01-03 14:46:47 +0200

    fix: debugging hasPkgChanged

--- scripts/ci/utils/hasPkgChanged.ts
+++ scripts/ci/utils/hasPkgChanged.ts
@@ -1,14 +1,16 @@
 import { execSync } from 'child_process'
-import path from 'path'
 
 // TODO: Must ignore files in __tests__ when deciding
 export const hasPkgChanged = (folder: string) => {
+  const ignore = ['__tests__']
   console.log(execSync(`git diff HEAD^ HEAD --name-only`).toString())
   return !!execSync(`git diff HEAD^ HEAD --name-only`)
     .toString()
     .trim()
     .split('\n')
-    .find((e) => e.includes(folder))
+    .find(
+      (e) =>
+        ignore.findIndex((item) => e.includes(item)) === -1 &&
+        e.includes(folder),
+    )
 }
-
-console.log(hasPkgChanged('./packages/cpg-github-actions'))
