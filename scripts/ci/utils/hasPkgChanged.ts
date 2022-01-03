6cd07f05ed034922fe9efd9daba39fe063556790
YassinEldeeb <yassineldeeb94@gmail.com>
2022-01-03 15:23:37 +0200

    fix: debugging hasPkgChanged

--- scripts/ci/utils/hasPkgChanged.ts
+++ scripts/ci/utils/hasPkgChanged.ts
@@ -3,6 +3,9 @@ import { execSync } from 'child_process'
 // TODO: Must ignore files in __tests__ when deciding
 export const hasPkgChanged = (folder: string) => {
   const ignore = ['__tests__']
+  console.log(
+    execSync(`git diff HEAD^ HEAD --name-only`).toString().trim().split('\n'),
+  )
   return !!execSync(`git diff HEAD^ HEAD --name-only`)
     .toString()
     .trim()
