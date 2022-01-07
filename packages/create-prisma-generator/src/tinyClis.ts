ffb86ee83ac4a876917ffc876e1eb15c8112f5b1
YassinEldeeb <yassineldeeb94@gmail.com>
2022-01-07 16:12:24 +0200

    fix: npm ignores some files when publishing

--- packages/create-prisma-generator/src/tinyClis.ts
+++ packages/create-prisma-generator/src/tinyClis.ts
@@ -1,20 +1,22 @@
+// No need to attach @latest at the end of the package name cause
+// npx will always execute the latest available version in the NPM registry
 export const CLIs = {
   typescriptTemplate(path: string) {
-    return `npx @cpg-cli/template-typescript@latest ${path}`
+    return `npx @cpg-cli/template-typescript ${path}`
   },
   rootConfigs(path: string) {
-    return `npx @cpg-cli/root-configs@latest ${path}`
+    return `npx @cpg-cli/root-configs ${path}`
   },
   usageTemplate(path: string) {
-    return `npx @cpg-cli/template-gen-usage@latest ${path}`
+    return `npx @cpg-cli/template-gen-usage ${path}`
   },
   javascriptTemplate(path: string) {
-    return `npx @cpg-cli/template@latest ${path}`
+    return `npx @cpg-cli/template ${path}`
   },
   githubActionsTemplate(path: string) {
-    return `npx @cpg-cli/github-actions@latest ${path}`
+    return `npx @cpg-cli/github-actions ${path}`
   },
   setupSemanticRelease(path: string, workspaceFlag: string) {
-    return `npx @cpg-cli/semantic-releases@latest ${path} ${workspaceFlag}`
+    return `npx @cpg-cli/semantic-releases ${path} ${workspaceFlag}`
   },
 }
