a5d7510b21f9a5363f8ad649872be379005fd5a4
YassinEldeeb <yassineldeeb94@gmail.com>
2022-01-07 15:27:24 +0200

    fix: try force-publishing [force publish]

--- scripts/ci/utils/hasPkgChanged.ts
+++ scripts/ci/utils/hasPkgChanged.ts
@@ -1,6 +1,13 @@
+import { execSync } from 'child_process'
+
 export const hasPkgChanged = (changes: string[], folder: string) => {
   const ignore = ['__tests__']
 
+  const lastCommitMessage = execSync('git show -s --format=%s').toString()
+
+  if (lastCommitMessage.includes('[force publish]')) {
+    return true
+  }
   return !!changes.find(
     (e) =>
       ignore.findIndex((item) => e.includes(item)) === -1 && e.includes(folder),
