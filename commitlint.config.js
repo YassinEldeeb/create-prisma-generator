0d789236d1cd7dd522487ba0ab46eaf87c61aad3
YassinEldeeb <yassineldeeb94@gmail.com>
2022-01-10 19:40:48 +0200

    style: adding blogs to commit message types

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,23 @@
+// Config commitlint rules:
+// https://github.com/conventional-changelog/commitlint/blob/master/docs/reference-rules.md
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'blogs',
+        'ci',
+        'refactor',
+        'perf',
+        'revert',
+        'style',
+        'test',
+        'docs',
+      ],
+    ],
+  },
 }
